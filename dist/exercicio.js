f6376fb7bd2d2558c45fa32e6d61697de5e84d99
LucasVTK <lucazim.vtk@gmail.com>
2025-03-10 15:03:18 -0300

    filtrando e exibindo alunos abaixo da média

--- dist/exercicio.js
+++ dist/exercicio.js
@@ -12,12 +12,21 @@ var arrayDeAlunos = [{
 }, {
   aluno: 'Matheus',
   nota: 6
+}, {
+  aluno: 'Pedro',
+  nota: 0
 }];
 function RetornaAlunoComNotaAcimaOuIgualA6() {
   for (var i = 0; i < arrayDeAlunos.length; i++) {
     if (arrayDeAlunos[i].nota >= 6) {
       console.log(arrayDeAlunos[i].aluno);
-    } else {}
+    }
   }
+  function filtrarAlunoAbaixoDaMedia(aluno) {
+    if (aluno.nota <= 5) {
+      console.log("aluno abaixo da m\xE9dia: ".concat(aluno.aluno));
+    }
+  }
+  var alunosAbaixo = arrayDeAlunos.filter(filtrarAlunoAbaixoDaMedia);
 }
 RetornaAlunoComNotaAcimaOuIgualA6();
